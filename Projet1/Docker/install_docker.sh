a7474932e9f20e2483b5c4db23a8fd51cbed57eb
AleChan16 <alechan.1693@gmail.com>
2026-04-26 20:09:07 +0200

    Add auto-confirm flag to Docker install command
    Add '-y' flag to Docker installation command to auto-confirm.

--- Projet1/Docker/install_docker.sh
+++ Projet1/Docker/install_docker.sh
@@ -22,4 +22,4 @@ EOF
 
 # Installer les paquets Docker
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
