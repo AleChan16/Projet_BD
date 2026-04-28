bd0c50c951254f22bf57cd79424b047403b119c0
Alejandra Diaz <alechan.1693@gmail.com>
2026-04-28 15:16:53 +0200

    Add sudo to MinIO CLI installation script

--- Projet1/Docker/install_minio_cli.sh
+++ Projet1/Docker/install_minio_cli.sh
@@ -1,4 +1,4 @@
-curl -L https://dl.min.io/client/mc/release/linux-amd64/mc --create-dirs  -o $HOME/minio-binaries/mc
-chmod +x $HOME/minio-binaries/mc
+sudo curl -L https://dl.min.io/client/mc/release/linux-amd64/mc --create-dirs  -o $HOME/minio-binaries/mc
+sudo chmod +x $HOME/minio-binaries/mc
 export PATH=$PATH:$HOME/minio-binaries/
 mc --version
