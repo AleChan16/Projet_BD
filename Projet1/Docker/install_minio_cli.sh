8aff7c454bf4c6b3a030c4b094f2374ed1ab57de
Alejandra Diaz <alechan.1693@gmail.com>
2026-04-28 16:33:03 +0200

    Add -L flag to curl command for MinIO CLI installation

--- Projet1/Docker/install_minio_cli.sh
+++ Projet1/Docker/install_minio_cli.sh
@@ -1,4 +1,4 @@
-curl https://dl.min.io/client/mc/release/linux-amd64/mc --create-dirs  -o $HOME/minio-binaries/mc
+curl -L https://dl.min.io/client/mc/release/linux-amd64/mc --create-dirs  -o $HOME/minio-binaries/mc
 chmod +x $HOME/minio-binaries/mc
 export PATH=$PATH:$HOME/minio-binaries/
 #echo export PATH=\$PATH:\$HOME/minio-binaries/ >> ~/.bashrc
